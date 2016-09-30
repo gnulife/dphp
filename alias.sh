7e0b4e7142c355c0c308edd2e16fd154df4ed0b6
GNULife <youwater@gmail.com>
2016-09-30 17:17:26 +0800

    增加mysql别名，方便本地连接调试

--- alias.sh
+++ alias.sh
@@ -1,2 +1,3 @@
 alias php="docker exec $(docker ps -a | grep phpfpm | cut -d ' ' -f 1) php"
 alias composer="docker exec $(docker ps -a | grep phpfpm | cut -d ' ' -f 1) composer"
+alias mysql="docker exec $(docker ps -a | grep mysql | cut -d ' ' -f 1) mysql"
