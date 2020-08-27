9e3e5b3064b24c5d7092055ce863ee029f448a88
water <youwater@qq.com>
2020-08-27 08:36:22 +0800

    修复phpfpm包，新增swoole alpine包

--- phpfpm/build.sh
+++ phpfpm/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 清除已有的
-docker stop phpfpm
-docker rm phpfpm
-docker rmi water/phpfpm 
+docker stop phpfpm7
+docker rm phpfpm7
+docker rmi water/phpfpm7 
 
 # 重新生成
-docker build -t water/phpfpm ./
+docker build -t water/phpfpm7 ./
--- phpfpm7/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 清除已有的
-docker stop phpfpm7
-docker rm phpfpm7
-docker rmi water/phpfpm7 
-
-# 重新生成
-docker build -t water/phpfpm7 ./
