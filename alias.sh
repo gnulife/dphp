b492e87d54b2820cbf784c8ca19ad75e93f64965
water <youwater@qq.com>
2020-08-28 14:27:24 +0800

    简化配置

--- alias.sh
+++ alias.sh
@@ -1,5 +1,5 @@
 alias php="docker exec $(docker ps -a | grep phpfpm | cut -d ' ' -f 1) php"
 # alias composer="docker exec $(docker ps -a | grep phpfpm | cut -d ' ' -f 1) composer"  有bug, 这样其实无法在host机上执行composer install
-alias composer='docker run --rm  -v `pwd`/:/current-dir -w /current-dir water/phpfpm7 composer' # 注意这里的单引号不要改动
+alias composer='docker run --rm  -v `pwd`/:/current-dir -w /current-dir water/phpfpm composer' # 注意这里的单引号不要改动
 alias mysql="docker exec -it $(docker ps -a | grep mysql | cut -d ' ' -f 1) mysql"
 alias redis-cli="docker exec -it $(docker ps -a | grep redis | cut -d ' ' -f 1) redis-cli"
